8ecc65e2441806d2408f468ed2d9e2c0e4678b07
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-07-03 09:40:13 -0300

    1st commit

---
+++ autonomous-car/arduino/LF_2W/LF_2W.ino
@@ -0,0 +1,253 @@
+#include <NewPing.h> //Biblioteca para uso das funções dos sensores ultrassônicos
+#include <Embedded_Protocol_2.h>
+#include <ArduinoJson.h>
+
+#define TRIGGER_PIN_RIGHT A0 //Sensor ultrassônico direito (vista traseira)
+#define ECHO_PIN_RIGHT A1    //Sensor ultrassônico direito (vista traseira)
+#define TRIGGER_PIN_LEFT A5  //Sensor ultrassônico esquerdo (vista traseira)
+#define ECHO_PIN_LEFT A4     //Sensor ultrassônico esquerdo (vista traseira)
+#define MAX_DISTANCE 200 //Máxima distância de leitura dos sensores (dois metros)
+
+// NewPing sonar_right(TRIGGER_PIN_RIGHT, ECHO_PIN_RIGHT, MAX_DISTANCE); //Função da biblioteca NewPing.h a qual se responsabiliza pela conversão das unidades
+// NewPing sonar_left(TRIGGER_PIN_LEFT, ECHO_PIN_LEFT, MAX_DISTANCE);    //Função da biblioteca NewPing.h a qual se responsabiliza pela conversão das unidades
+
+int IN[4] = {5, 6, 10, 11}; //Definindo pinos dos motores
+// #define PH1 4
+// #define PH2 5
+// #define PH3 6
+// #define PH4 7
+
+int lumi_esq = 0;           //Necesidade de ser uma variável global
+int lumi_dir = 0;           //Necesidade de ser uma variável global
+int lumi_mark = 0;
+
+Communication com;
+
+int time = 5000;
+
+/* Devido ao fato de terem muitos sensores conectados ao Vcc e o arduíno possuir apenas um pino para isto
+defini-se o pino 13 em nível lógico alto, apenas para distribuir as cargas */
+
+void setup() {
+
+  for(int i = 0; i <= 4; i++){
+    pinMode(IN[i], OUTPUT);
+    digitalWrite(IN[i], LOW);
+  }
+
+  pinMode(lumi_esq, INPUT);
+  pinMode(lumi_dir, INPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  while(Serial.available() > 0) {
+    String s = Serial.readString();
+    if(s.equals("move_front")){
+      front();
+
+    } else if(s.equals("move_left")){
+      d_left();
+
+    } else if(s.equals("move_right")){
+      d_right();
+
+    } 
+    // else if(s.equals("halt_turn")){
+
+    //   com.startBelief("inspec");
+    //   com.beliefAdd(s);
+    //   com.endBelief();
+
+    //   com.sendMessage();
+
+    //   d_stop();
+    // }
+    // com.startBelief("inspec");
+    // com.beliefAdd(s);
+    // com.endBelief();
+
+    // com.sendMessage();
+  }
+
+}
+  
+void front(){
+
+  bool frwd = true;
+
+  com.startBelief("front");
+  com.beliefAdd("in");
+  com.endBelief();
+  com.sendMessage();
+
+  while(frwd == true){
+
+    lumi_esq = digitalRead(9);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+    lumi_dir = digitalRead(8);           //Variável de medida do sensor de luminosidade direito (vista traseira)
+
+    if(lumi_esq && lumi_dir){ //Caso ambos sensores estejam identificando luminosidade
+      move_front();                   //O carrinho se locomove para frente
+    }
+    else if(!lumi_esq && lumi_dir){ //Caso o sensor da direita identifique luminosidade
+      move_right();                        //O carrinho se move para a esquerda, afim de encontrar a linha
+    }
+    else if(lumi_esq && !lumi_dir){ //Caso o sensor da esquerda identifique luminosidade
+      move_left();                       //O carrinho se move para a direita, afim de encontrar a linha
+    }   
+
+    lumi_mark = digitalRead(A3);          //Variável digital de detecção de marcador (sensor de decisao)0
+
+    if(lumi_mark){
+      while(frwd == true){ // o carrinho precisa ir para frente até o ponto de decisão
+        move_front();
+
+        lumi_esq = digitalRead(9);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+        lumi_dir = digitalRead(8);           //Variável de medida do sensor de luminosidade direito (vista traseira)
+
+        if (!lumi_esq && !lumi_dir){
+          frwd = false;
+        }
+      }
+      delay(500);
+      halt();
+      break;             //O agente toma uma decisao
+    }
+  }
+
+  com.startBelief("comm");
+  com.beliefAdd("ahead");
+  com.endBelief();
+
+  com.sendMessage();
+
+}
+
+void d_left(){
+
+  com.startBelief("insp");
+  com.beliefAdd("t2");
+  com.endBelief();
+  com.sendMessage();
+
+  while(!lumi_dir){
+    lumi_dir = digitalRead(8);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+
+    digitalWrite(IN[0], HIGH);
+    digitalWrite(IN[1], LOW);
+    digitalWrite(IN[2], LOW);
+    digitalWrite(IN[3], HIGH);
+
+  }
+
+  halt();
+
+  com.startBelief("comm");
+  com.beliefAdd("ahead");
+  com.endBelief();
+  com.sendMessage();
+
+ }
+
+void d_right(){
+
+  com.startBelief("insp");
+  com.beliefAdd("t3");
+  com.endBelief();
+  com.sendMessage();
+
+
+  while(!lumi_esq){
+    lumi_esq = digitalRead(9);         //Variável de medida do sensor de luminosidade direito (vista traseira)
+
+    digitalWrite(IN[0], LOW);
+    digitalWrite(IN[1], HIGH);
+    digitalWrite(IN[2], HIGH);
+    digitalWrite(IN[3], LOW);
+
+  }
+
+  halt();
+
+  com.startBelief("comm");
+  com.beliefAdd("ahead");
+  com.endBelief();
+  com.sendMessage();
+
+ }
+
+//  void d_stop(){
+  
+//   bool frwd = true, flag = true;
+
+//   while(flag){
+//     digitalWrite(IN[0], LOW);
+//     digitalWrite(IN[1], HIGH);
+//     digitalWrite(IN[2], HIGH);
+//     digitalWrite(IN[3], LOW);
+
+//     lumi_esq = digitalRead(9);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+
+//     if(!lumi_esq){flag=false;}
+//   }
+
+//   halt();
+
+//   com.startBelief("comm");
+//   com.beliefAdd("turn");
+//   com.endBelief();
+//   com.sendMessage();
+
+//  }
+
+//ABAIXO CONSTAM APENAS AS FUNÇÕES DE SENTIDO DE GIRO E COMBINAÇÃO ENTRE OS MOTORES
+
+void move_front(){
+
+  digitalWrite(IN[0], LOW);
+  digitalWrite(IN[1], HIGH);
+  digitalWrite(IN[2], LOW);
+  digitalWrite(IN[3], HIGH);
+
+  // digitalWrite(PH1, HIGH);
+  // digitalWrite(PH2, LOW); // move pneu direita para frente
+  // digitalWrite(PH2, HIGH);
+  // digitalWrite(PH4, LOW);
+
+  // Serial.println("forward");
+
+}
+
+void halt(){
+  digitalWrite(IN[0], LOW);
+  digitalWrite(IN[1], LOW);
+  digitalWrite(IN[2], LOW);
+  digitalWrite(IN[3], LOW);
+  // Serial.println("halt");
+}
+
+void move_back(){
+  digitalWrite(IN[0], HIGH);
+  digitalWrite(IN[1], LOW);
+  digitalWrite(IN[3], HIGH);
+  digitalWrite(IN[2], LOW);
+}
+
+void move_right(){
+  digitalWrite(IN[0], LOW);
+  digitalWrite(IN[1], HIGH);
+  digitalWrite(IN[2], LOW);
+  digitalWrite(IN[3], LOW);
+  // Serial.println("right");
+}
+
+void move_left(){
+  digitalWrite(IN[0], LOW);
+  digitalWrite(IN[1], LOW);
+  digitalWrite(IN[2], LOW);
+  digitalWrite(IN[3], HIGH);
+  // Serial.println("left");
+}
---
+++ autonomous-car/arduino/LF_4W/LF_4W.ino
@@ -0,0 +1,202 @@
+#include<Embedded_Protocol_2.h>
+#include<ArduinoJson.h>
+//#include<NewPing.h>
+#include<AFMotor.h>
+
+//DEFININDO VARIÁVEIS MOTORES
+AF_DCMotor motorFL(1);
+AF_DCMotor motorFR(4);
+AF_DCMotor motorBL(2);
+AF_DCMotor motorBR(3);
+int lumi_esq = 0;           //Necesidade de ser uma variável global
+int lumi_dir = 0;          //Necesidade de ser uma variável global
+int lumi_mark = 0;
+
+int time = 5000; // tempo que o agente espera antes da proxima instrucao
+
+// noDelay BeliefTime(500); //tempo do envio das crenças
+Communication com;
+
+/* Devido ao fato de terem muitos sensores conectados ao Vcc e o arduíno possuir apenas um pino para isto
+defini-se o pino 13 em nível lógico alto, apenas para distribuir as cargas */
+
+void setup() {
+
+  //VELOCIDADE DOS MOTORES -> AJUSTÁVEL CASO NÃO ESTEJAM SINCRONIZADOS (255 = Vmáx)
+  motorFR.setSpeed(255);
+  motorBR.setSpeed(255);
+  motorFL.setSpeed(255);
+  motorBL.setSpeed(255);
+
+  halt(); //OS MOTORES INICIAM PARADOS
+
+  Serial.begin(9600);
+
+  delay(3000);
+
+}
+
+void loop() {
+
+  while (Serial.available() > 0) { // checa presenca de informacao na serial
+    String s = Serial.readString();
+    if(s.equals("move_front")){
+      front(); // caso agente envie 'front', arduino move o robo para frente
+
+    } else if(s.equals("move_left")){
+      d_left();   // caso agente envie 'left', arduino move o robo para esquerda
+
+    } else if(s.equals("move_right")){
+      d_right();  // caso agente envie 'right', arduino move o robo para direita
+      
+    }
+  }
+ }
+
+void front(){
+
+  bool frwd = true;
+
+  char comm = "wait";
+  com.startBelief("wait");
+  com.beliefAdd(comm);
+  com.endBelief();
+
+  com.sendMessage();
+
+  while(frwd){
+
+    lumi_esq = digitalRead(A4);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+    lumi_dir = digitalRead(A2);           //Variável de medida do sensor de luminosidade direito (vista traseira)
+    lumi_mark = digitalRead(9);          //Variável digital de detecção de marcador (sensor de decisao)
+
+    if(lumi_esq && lumi_dir){ //Caso ambos sensores estejam identificando luminosidade
+      moveForward();                   //O carrinho se locomove para frente
+    }
+    else if(!lumi_esq && lumi_dir){ //Caso o sensor da direita identifique luminosidade
+      moveRight();                        //O carrinho se move para a esquerda, afim de encontrar a linha
+    }
+    else if(lumi_esq && !lumi_dir){ //Caso o sensor da esquerda identifique luminosidade
+      moveLeft();                       //O carrinho se move para a direita, afim de encontrar a linha
+    }   
+
+    if(lumi_mark){
+      while(frwd){ // o carrinho precisa ir para frente até o ponto de decisão
+        moveForward();
+        if(!lumi_esq && !lumi_dir!){
+          frwd = false;
+        }
+      }
+      delay(500);
+      halt();              //O agente toma uma decisao
+    }
+  }
+
+  comm = "ahead";
+  com.startBelief("ahead");
+  com.beliefAdd(comm);
+  com.endBelief();
+
+  com.sendMessage();
+
+ }
+
+ void d_left(){
+
+  com.startBelief("wait");
+  com.beliefAdd(time);
+  com.endBelief();
+
+  com.sendMessage();
+
+  while(lumi_dir == 0){
+    lumi_dir = digitalRead(A4);           //Variável de medida do sensor de luminosidade esquerdo (vista traseira)
+
+    motorBL.run(BACKWARD);
+    motorFL.run(BACKWARD);
+    // motorBL.run(RELEASE);
+    // motorFL.run(RELEASE);
+    motorBR.run(FORWARD);
+    motorFR.run(FORWARD);
+
+    Serial.println("Decision Left");
+  }
+
+ }
+
+ void d_right(){
+
+  com.startBelief("wait");
+  com.beliefAdd(time);
+  com.endBelief();
+
+  com.sendMessage();
+
+  while(lumi_esq == 0){
+    lumi_esq = digitalRead(A2);         //Variável de medida do sensor de luminosidade direito (vista traseira)
+
+    motorBR.run(BACKWARD);
+    motorFR.run(BACKWARD);
+    // motorBR.run(RELEASE);
+    // motorFR.run(RELEASE);
+    motorBL.run(FORWARD);
+    motorFL.run(FORWARD);
+
+    Serial.println("Decision Right");
+  }
+
+ }
+
+
+void moveForward(){
+
+  motorBL.run(FORWARD);
+  motorFL.run(FORWARD);
+  motorBR.run(FORWARD);
+  motorFR.run(FORWARD);
+
+  Serial.println("forward");
+
+}
+
+void moveBackward(){
+
+  motorBL.run(BACKWARD);
+  motorFL.run(BACKWARD);
+  motorBR.run(BACKWARD);
+  motorFR.run(BACKWARD);
+
+  Serial.println("backward");
+
+}
+
+void moveLeft(){
+
+  motorBL.run(RELEASE);
+  motorFL.run(RELEASE);
+  motorBR.run(FORWARD);
+  motorFR.run(FORWARD);
+
+  Serial.println("left");
+
+}
+
+void moveRight(){
+
+  motorBR.run(RELEASE);
+  motorFR.run(RELEASE);
+  motorBL.run(FORWARD);
+  motorFL.run(FORWARD);
+
+  Serial.println("right");
+
+}
+
+void halt(){
+
+  motorBR.run(RELEASE);
+  motorFR.run(RELEASE);
+  motorBL.run(RELEASE);
+  motorFL.run(RELEASE);
+
+}
