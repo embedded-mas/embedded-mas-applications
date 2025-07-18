e77f071a0197acbf43d2114f2788be5f781ae8ee
maiquelb <maiquelb@gmail.com>
2025-07-18 13:58:07 -0300

    update car4wheels multi-device

--- collision-avoiding-vehicle/car4wheels-multi-device/arduino/4wheels/4wheels.ino
+++ collision-avoiding-vehicle/car4wheels-multi-device/arduino/4wheels/4wheels.ino
@@ -14,7 +14,9 @@ Communication communication;
 
 int leitura = 1;
 int sensor_state = 5;
-int dist_sensor_1, dist_sensor_2;
+float dist_sensor_1, //left distance sensor
+    dist_sensor_2; //right distance sensor
+
 
 NewPing sonar_1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
 NewPing sonar_2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
@@ -25,8 +27,12 @@ AF_DCMotor motorFR(4);
 AF_DCMotor motorBL(2);
 AF_DCMotor motorBR(3);
 
+
 void setup() 
 {
+
+
+  randomSeed(analogRead(0));
 
   //VELOCIDADE DOS MOTORES -> AJUSTÁVEL CASO NÃO ESTEJAM SINCRONIZADOS (255 = Vmáx)
   motorFR.setSpeed(255);
@@ -49,14 +55,10 @@ void setup()
 
 void loop() 
 {
-  dist_sensor_1 = sonar_1.ping_cm();
-  dist_sensor_2 = sonar_2.ping_cm();
+  readSensors();
 
-  distance_correction();
-  //Serial.print("Sensor 1:");
-  //Serial.println(dist_sensor_1);
-  Serial.print("Sensor 2:");
-  Serial.println(dist_sensor_2);
+  //distance_correction();
+
 
   while(Serial.available() > 0){ //check whether there is some information from the serial (possibly from the agent)
     String s = Serial.readString();
@@ -105,92 +107,180 @@ void loop()
     leitura = 1;
   }
 
+  
+  
   if(leitura == 1){
-    //*****SE NÃO ENCONTRAR NADA A 20 CM DOS SENSORES, ENTÃO SEGUE*****
-    if(dist_sensor_1 >= 20 && dist_sensor_2 >= 20 && sensor_state != 1){
-      sensor_state = 1;
-      leitura = 0;
-      //pode seguir em frente
-      communication.startBelief("conditions");
-      communication.beliefAdd(3);
-      communication.endBelief();
-      communication.sendMessage();
-    }
-    //*****SE ENCONTRAR ALGO PŔOXIMO, ENTÃO PARA E VERIFICA*****
-    else if(dist_sensor_1 < 20 && sensor_state != 0 || dist_sensor_2 < 20 && sensor_state != 0){
-      sensor_state = 0;
-      leitura = 0;
-
-      int dist_difference = dist_sensor_1 - dist_sensor_2;
-
-      if(dist_difference > 5){
-        //move-se para um lado
-        communication.startBelief("conditions");
-        communication.beliefAdd(2);
-        communication.endBelief();
-        communication.sendMessage();
-      }
-      else if(dist_difference < -5){
-        //move-se para o outro lado
-        communication.startBelief("conditions");
-        communication.beliefAdd(1);
-        communication.endBelief();
-        communication.sendMessage();
-      }
-      else{
-        //obstáculo nos dois sensores com msm distancia
-        communication.startBelief("conditions");
-        communication.beliefAdd(0);
-        communication.endBelief();
-        communication.sendMessage();
-      }
-    }
+     generatePercepts();
   }
   
 }
 
+
+/*
+ * TODO: Implementar leitura dos sensores
+ */
+void readSensors(){
+   long d1, d2;       
+   d1 = random(100); Serial.println(d1);
+   d2 = random(100); Serial.println(d2);
+
+   dist_sensor_1 = d1/100.0; Serial.println(dist_sensor_1);
+   dist_sensor_2 = d2/100.0; Serial.println(dist_sensor_2);
+
+   delay(5000); //espera 5 secs para rodar experimentos sem o carrinho
+}
+
+
+
+void generatePercepts(){
+  
+   communication.startBelief("dist_left");
+   communication.beliefAdd(dist_sensor_1);
+   communication.endBelief();
+
+   communication.startBelief("dist_right");
+   communication.beliefAdd(dist_sensor_2);
+   communication.endBelief();
+   communication.sendMessage();
+
+  
+//    //*****SE NÃO ENCONTRAR NADA A 20 CM DOS SENSORES, ENTÃO SEGUE*****
+//    if(dist_sensor_1 >= 20 && dist_sensor_2 >= 20 && sensor_state != 1){
+//      sensor_state = 1;
+//      leitura = 0;
+//      //pode seguir em frente
+//      communication.startBelief("conditions");
+//      communication.beliefAdd(3);
+//      communication.endBelief();
+//      communication.sendMessage();
+//    }
+//    
+//    //*****SE ENCONTRAR ALGO PŔOXIMO, ENTÃO PARA E VERIFICA*****
+//    else if(dist_sensor_1 < 20 && sensor_state != 0 || dist_sensor_2 < 20 && sensor_state != 0){
+//      sensor_state = 0;
+//      leitura = 0;
+//
+//      int dist_difference = dist_sensor_1 - dist_sensor_2;
+//
+//      if(dist_difference > 5){
+//        //move-se para um lado
+//        communication.startBelief("conditions");
+//        communication.beliefAdd(2);
+//        communication.endBelief();
+//        communication.sendMessage();
+//      }
+//      else if(dist_difference < -5){
+//        //move-se para o outro lado
+//        communication.startBelief("conditions");
+//        communication.beliefAdd(1);
+//        communication.endBelief();
+//        communication.sendMessage();
+//      }
+//      else{
+//        //obstáculo nos dois sensores com msm distancia
+//        communication.startBelief("conditions");
+//        communication.beliefAdd(0);
+//        communication.endBelief();
+//        communication.sendMessage();
+//      }
+//    }
+}
+
+
+void motorFL_Forward(){
+   motorFL.run(FORWARD);
+}
+
+void motorFL_Backward(){
+   motorFL.run(BACKWARD);
+}
+
+void motorFL_Release(){
+   motorFL.run(RELEASE);
+}
+
+void motorFR_Forward(){
+   motorFR.run(FORWARD);
+}
+
+void motorFR_Backward(){
+   motorFR.run(BACKWARD);
+}
+
+void motorFR_Release(){
+   motorFR.run(RELEASE);
+}
+
+
+void motorBL_Forward(){
+   motorBL.run(FORWARD);
+}
+
+void motorBL_Backward(){
+   motorFL.run(BACKWARD);
+}
+
+void motorBL_Release(){
+   motorBL.run(RELEASE);
+}
+
+
+void motorBR_Forward(){
+   motorBR.run(FORWARD);
+}
+
+void motorBR_Backward(){
+   motorBR.run(BACKWARD);
+}
+
+
+void motorBR_Release(){
+   motorBR.run(RELEASE);
+}
+
+
 void moveForward(){
 
-  motorBL.run(FORWARD);
-  motorFL.run(FORWARD);
-  motorBR.run(FORWARD);
-  motorFR.run(FORWARD);
+  motorBL_Forward();
+  motorFL_Forward();
+  motorBR_Forward();
+  motorFR_Forward();
 
 }
 
 void moveBackward(){
 
-  motorBL.run(BACKWARD);
-  motorFL.run(BACKWARD);
-  motorBR.run(BACKWARD);
-  motorFR.run(BACKWARD);
+  motorBL_Backward();
+  motorFL_Backward();
+  motorBR_Backward();
+  motorFR_Backward();
 
 }
 
 void moveRight(){
 
-  motorBL.run(RELEASE);
-  motorFL.run(RELEASE);
-  motorBR.run(FORWARD);
-  motorFR.run(FORWARD);
+  motorBL_Release();
+  motorFL_Release();
+  motorBR_Forward();
+  motorFR_Forward();
 
 }
 
 void moveLeft(){
 
-  motorBR.run(RELEASE);
-  motorFR.run(RELEASE);
-  motorBL.run(FORWARD);
-  motorFL.run(FORWARD);
+  motorBR_Release();
+  motorFR_Release;
+  motorBL_Forward();
+  motorFL_Forward();
 
 }
 
 void halt(){
 
-  motorBR.run(RELEASE);
-  motorFR.run(RELEASE);
-  motorBL.run(RELEASE);
-  motorFL.run(RELEASE);
+  motorBR_Release();
+  motorFR_Release();
+  motorBL_Release();
+  motorFL_Release();
 
 }
 
--- collision-avoiding-vehicle/car4wheels-multi-device/src/agt/sample_agent.asl
+++ collision-avoiding-vehicle/car4wheels-multi-device/src/agt/sample_agent.asl
@@ -1,24 +1,66 @@
-// CARRO ATUADO POR QUATRO RODAS CONTROLADAS
+// obstacle_front(L) :- dist_left(L) & dist_right(R) & L<=0.2 & R <=0.2 & L < R.
 
-+init(1)
-   <- .print("Iniciando...").
-
-+conditions(3)
-   <- .print("Não há objetos a frente...\nLigando motores");
-       embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","Front",[]).
-
-+conditions(2)
-   <- .print("Virando para a Direita...");
-       embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","Left",[]).
-
-+conditions(1)
-   <- .print("Virando para a Esquerda...");
-       embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","Right",[]).
-
-+conditions(0)
-   <- .print("Objeto muito próximo! Retrocedendo...");
-       embedded.mas.bridges.jacamo.defaultEmbeddedInternalAction("arduino1","Back",[]).
+// obstacle_front(R) :- dist_left(L) & dist_right(R) & L<=0.2 & R <=0.2 & L >= R.
 
 
+// obstacle_right(R) :- dist_left(L) & dist_right(R) & L>0.2 & R <=0.2.
 
 
+// obstacle_left(L) :- dist_left(L) & dist_right(R) & L<=0.2 & R>0.2.
+
+
+// +dist_left(L) : obstacle_front(X)  & dist_right(R) 
+//    <- .print("obstacle front ", X, " left: ", L, " right: ", R).
+
+// +dist_left(L) : obstacle_left(L)  & dist_right(R) 
+//    <- .print("obstacle  left: ", L, " right: ", R).   
+
+// +dist_left(L) : obstacle_right(R)  & dist_right(R) 
+//    <- .print("obstacle  right: ", L, " right: ", R).      
+   
++dist_left(L) 
+   <- .print("Distance LEFT: ", L).
+
++dist_right(L) 
+   <- .print("Distance RIGHT: ", L).   
+
+
+
+ !walk.
+
++!walk : obstacle_front(F) & F < 1 
+   <- .print("Obstacle front") ;
+      .back;
+      .wait(1000);      //tive que ajustar o tempo para 1000 para dar tempo de o arduino processar
+      .right;
+       .wait(1000);
+      .print("Indo para trás");
+      !walk.   
+
++!walk : obstacle_left(L) & L < 0.2 &
+         (not obstacle_right(_) | obstacle_right(R) & R > L)
+   <- .print("Obstacle left. Virando a direita ", L, ",", R) ;      
+      .right;
+      //.print("Virando a direita");
+      .wait(100);
+      !walk.
+
++!walk : obstacle_right(R) & R < 0.2 &
+         (not obstacle_left(_) | obstacle_left(L) & L > R)
+   <- .print("Obstacle right. Virando a esquerda", L, ",", R) ;
+      .left;
+      //.print("Virando a esquerda");
+      .wait(100);
+      !walk.
+
+
+   
+
++!walk 
+   <- .print("No obstacle. Moving forward");      
+      .front;
+      //.print("Indo em frente");
+      .wait(100);
+      !walk;
+      .
+         
